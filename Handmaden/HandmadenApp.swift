9ad76e1a1fa145915b6952ba04507293f576cc12
axfng <alexlfeng22@gmail.com>
2024-11-02 14:29:12 -0400

    Initial Commit

---
+++ Handmaden/HandmadenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandmadenApp.swift
+//  Handmaden
+//
+//  Created by alfeng on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HandmadenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
