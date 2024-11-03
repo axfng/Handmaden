92388d1552c072c3175c862630490489a06253b5
axfng <alexlfeng22@gmail.com>
2024-11-03 01:44:48 -0400

    Implement SavedView

--- Handmaden/Features/Saved/Views/SavedView.swift
+++ Handmaden/Features/Saved/Views/SavedView.swift
@@ -13,9 +13,36 @@ struct SavedView: View {
     @EnvironmentObject private var cartViewModel: CartViewModel
     @EnvironmentObject var savedViewModel: SavedViewModel
 
-
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Saved Items")
+                    .font(.title.bold())
+                    .foregroundStyle(.blue)
+                    .padding()
+            }
+            if savedViewModel.savedItems.isEmpty {
+                Text("No liked items... yet!")
+                    .foregroundStyle(.gray)
+                    .padding()
+            } else {
+                List(savedViewModel.savedItems) { product in
+                    ProductCardView(product: product)
+                        .swipeActions {
+                            Button(role: .destructive) {
+                                savedViewModel.toggleLike(for: product)
+                            } label: {
+                                Label("Remove", systemImage: "trash")
+                            }
+                        }
+                }
+                .listStyle(InsetGroupedListStyle())
+            }
+            
+            Spacer()
+            
+        }
+        .navigationTitle("Saved")
     }
 }
 
